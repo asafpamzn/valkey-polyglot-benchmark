36fe5e328c8c5420b0f033ed6746d88c27702cab
asafpamzn <asafp@amazon.com>
2026-05-24 12:55:17 +0300

    data integrity scenarios

--- python/scenarios/large_scenario10_integrity.sh
+++ python/scenarios/large_scenario10_integrity.sh
@@ -26,7 +26,7 @@ trap cleanup INT TERM EXIT
 # Parse arguments
 SKIP_WARMUP=false
 SKIP_TRAFFIC=false
-HOST=""
+HOST="ec2-98-80-5-25.compute-1.amazonaws.com"
 REPLICA_HOST=""
 USE_TLS=true
 while [[ $# -gt 0 ]]; do
--- python/scenarios/large_scenario10_integrity_migrate.sh
+++ python/scenarios/large_scenario10_integrity_migrate.sh
@@ -26,7 +26,7 @@ trap cleanup INT TERM EXIT
 # Parse arguments
 SKIP_WARMUP=false
 SKIP_TRAFFIC=false
-HOST=""
+HOST="ec2-98-80-5-25.compute-1.amazonaws.com"
 REPLICA_HOST=""
 PRIMARY_SSH="ssh -i ~/.ssh/mac.pem ubuntu@ec2-98-80-5-25.compute-1.amazonaws.com"
 MIGRATE_SCRIPT="~/work/criu/scripts/migrate_new.sh"
